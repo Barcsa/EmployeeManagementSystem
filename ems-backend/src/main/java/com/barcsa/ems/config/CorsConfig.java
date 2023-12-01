34beca9e7bbf22d97c2d7b2a974346b089dd2926
Barcsa <barcsathemiddleman@gmail.com>
2023-12-02 00:23:35 +0200

    config change

--- ems-backend/src/main/java/com/barcsa/ems/config/CorsConfig.java
+++ ems-backend/src/main/java/com/barcsa/ems/config/CorsConfig.java
@@ -7,13 +7,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-        @Override
+    @Autowired
+    private CatchAllInterceptor catchAllInterceptor;
+
+    @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
                     .allowedOrigins("http://localhost:3000")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("Authorization", "Content-Type", "Accept");
         }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(catchAllInterceptor);
+    }
     }
 
 
